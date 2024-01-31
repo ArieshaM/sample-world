51c2056f4690cd35d43dafd7de245e7df216115b
Ariesha Murray <murrayariesha@gmail.com>
2024-01-31 02:37:45 -0600

    made changes to the front page

--- app.js
+++ app.js
@@ -2,14 +2,42 @@
 
 console.log ("You Can Have Confidence");
 alert ("Confidence starts here!");
+youCanhaveconfidence();
 
-const confidenceRating = prompt ("How would you rate your confidence level today?");
+function youCanhaveconfidence() {
+    document.write("Your" + confidenceRating + " Adds value to who you are");
+}
+
+function getConfidenceRating() {
+    const confidenceRating = prompt ("How would you rate your confidence level today?")
+        return confidenceRating;
+}
+
+// const confidenceRating = getConfidenceRating();
+
+
+// console.log (getConfidenceRating);
+// 
 console.log (confidenceRating);
 
 // string concatenation means strings combined with variables.
-document.write("Your "  + confidenceRating +   " Adds value to who you are");
 
-if (confidenceRating == "5") {alert ("Take our Confidence Course");
+
+
+
+document.write("Your "  + confidenceRating +   " Adds value to who you are");
+youCanhaveconfidence();
+
+function specialmessage(confidenceRating){
+  if (confidenceRating == "")
+  
+     confidenceRating = prompt("Come on, please tell me your confidence rating!");}
+     if (confidenceRating == "5") 
+document.write ("Take our Confidence Course"); 
+
+if (confidenceRating == "5") {alert ("Take our Confidence Course"); {
+    
+}
 } else if (confidenceRating == "7") { 
     alert ("Get our Confidence Ebook");
 }else if (confidenceRating == "1") {
@@ -21,3 +49,27 @@ if (confidenceRating == "5") {alert ("Take our Confidence Course");
 else {
     alert ("Take our Confidence Course")
 }
+
+
+
+
+// FUNCTIONS!!
+
+// function addTwoNumbers() {
+//     let num1 = 7;
+//     let num2 = 4;
+//     let sum = num1 + num2;
+//     console.log(sum);
+//     return (sum);
+
+// }
+
+// function addTwoNumbers(num1, num2) {
+//     let sum= num1 + num2;
+//     console.log("Sum is: ", sum);
+//     return sum;
+// }
+
+// addTwoNumbers (47,20);
+// addTwoNumbers (1,4); 
+
