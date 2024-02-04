0bacfef7ba55bccfdbb55c2ec493732502a8b983
Ariesha Murray <murrayariesha@gmail.com>
2024-02-03 19:14:42 -0600

    made changes to front page

--- app.js
+++ app.js
@@ -1,68 +1,81 @@
 'use strict';
 
-console.log ("You Can Have Confidence");
-alert ("Confidence starts here!");
-youCanHaveConfidence();
+console.log("You Can Have Confidence");
+alert("Confidence starts here!");
 
-function youCanHaveConfidence() {
-    document.write("Your" + confidenceRating + " Adds value to who you are");
+
+    let response = prompt('Do you have confidence? Answer yes to proceed') ;
+
+    while (response != "yes" ) {
+        response = prompt ('please answer yes to proceed') ;
+
+    }
+
+    alert ('Check out our Confidence Ebook');
+
+function youCanHaveConfidence(confidenceRating) {
+    if (confidenceRating == "") {
+        confidenceRating = prompt("Come on, please tell me your confidence rating!");
+    } else if (confidenceRating == "5") {
+        alert("Take our Confidence Course");
+    } else if (confidenceRating == "7") {
+        alert("Get our Confidence Ebook");
+    } else if (confidenceRating == "1") {
+        alert("Take our Confidence Course");
+    } else if (confidenceRating == "3") {
+        alert("Take our Confidence Course");
+    } else if (confidenceRating == "10")
+        alert("Get our Confidence Ebook");
+    else {
+        alert("Take our Confidence Course")
+    }
+    document.write("Your " + confidenceRating + " Adds value to who you are");
 }
 
-function getConfidenceRating() {
-    const confidenceRating = prompt ("How would you rate your confidence level today?")
+    function getConfidenceRating() {
+        const confidenceRating = prompt("How would you rate your confidence level today?")
         return confidenceRating;
-}
+    }
 
-// const confidenceRating = getConfidenceRating();
-
-
-// console.log (getConfidenceRating);
-// 
-// console.log (confidenceRating);
-
-// string concatenation means strings combined with variables.// 
+    const confidenceRating = getConfidenceRating();
 
 
-// document.write("Your "  + confidenceRating +   " Adds value to who you are");
-youCanHaveConfidence();
+    // console.log (getConfidenceRating);
+    // 
+    // console.log (confidenceRating);
 
-function specialmessage(confidenceRating){
-    if (confidenceRating == ""){
-     confidenceRating = prompt("Come on, please tell me your confidence rating!");}
-     
-     if (confidenceRating == "5");
-     document.write ("Take our Confidence Course");
-if (confidenceRating == "5") {alert ("Take our Confidence Course");
-} else if (confidenceRating == "7") { 
-    alert ("Get our Confidence Ebook");
-} else if (confidenceRating == "1") {
-        alert ("Take our Confidence Course");
-} else if (confidenceRating == "3") {
-    alert ("Take our Confidence Course");
-} else if (confidenceRating == "10") 
-    alert ("Get our Confidence Ebook");
-  else {
-    alert ("Take our Confidence Course")
-}
+    // string concatenation means strings combined with variables.// 
 
 
+    // document.write("Your "  + confidenceRating +   " Adds value to who you are");
+    youCanHaveConfidence(confidenceRating);
+
+    function specialmessage(confidenceRating) {
+        if (confidenceRating == "") {
+            confidenceRating = prompt("Come on, please tell me your confidence rating!");
+        }
 
 
-// FUNCTIONS!!
+    }
 
-// function addTwoNumbers() {
-//     let num1 = 7;
-//     let num2 = 4;
-//     let sum = num1 + num2;
-//     console.log(sum);
-//     return (sum);
 
-// }
+    // let response = prompt('Do you have confidence? Answer yes to proceed') ;
 
-// function addTwoNumbers(num1, num2) {
-//     let sum= num1 + num2;
-//     console.log("Sum is: ", sum);
-//     return sum;
-// }
+    // while (response != "yes" ) {
+    //     response = prompt ('please answer yes to proceed') ;
 
-// addTwoNumbers (47,20);// addTwoNumbers (1,4);}
+    // }
+
+    // alert (' Thank you! Check out our Confidence Ebook');
+
+        function rateMyPage(){
+        let rating = prompt ("How many stars would you rate my page? 1-5");
+// https://canvas.instructure.com/courses/6652008/grades
+        for (let i = 0; 1 < rating; i++) {
+            document.write ("https://unsplash.com/photos/selective-focus-photography-of-jolly-woman-using-peace-hand-gesture-oTweoxMKdkA") ;
+        }
+
+    }
+    
+    rateMyPage()
+
---
+++ win.js
@@ -0,0 +1,8 @@
+let result = 8 * 3;
+
+let y = 3 + 4;
+
+let variable a equal = "Welcome John Doe";
+
+let x = "Failing is just another word for learning.";
+
